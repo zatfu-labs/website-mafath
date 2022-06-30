fb5a883ff983c2fc240d057f960a20e8d6dda040
Fauzaro01 <MuhamadFauzan4750@gmail.com>
2022-06-30 15:01:31 +0700

    rename file

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const apiRouter = require('./routes/api');
 
 const app = express();
 var port = process.env.PORT || 3000;
@@ -25,23 +24,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('*', (req, res) => {
+  res.send({
+    code: 404,
+    message: 'salah arah lu bang -_-'
+  })
+})
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
